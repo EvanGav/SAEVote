97f45639b46e889c8b5398ef358b4c4666d5e8f2
EvanGav <34816766+EvanGav@users.noreply.github.com>
2023-02-04 23:41:18 +0100

    Add files via upload

---
+++ client/src/main/java/com/clochelabs/ConfServer.java
@@ -0,0 +1,48 @@
+package com.clochelabs;
+
+public class ConfServer {
+
+    private static String address = "127.0.0.1";
+
+
+    private static int port = 5056;
+
+    public static String getAddress() {
+        System.out.println(address);
+        return address;
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 82.66.50.92
+}
